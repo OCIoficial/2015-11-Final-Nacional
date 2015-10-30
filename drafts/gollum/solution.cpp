51706666c1fb3f2d15fd6afb841e074669e6b8c4
Nico Lehmann <nlehmann@dcc.uchile.cl>
2015-10-30 16:07:32 -0300

    drafts

---
+++ drafts/gollum/solution.cpp
@@ -0,0 +1,42 @@
+/*
+ * -----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <nlehmann@dcc.uchile.cl> wrote this file. As long as you retain this notice
+ * you can do whatever you want with this stuff. If we meet some day, and you
+ * think this stuff is worth it, you can buy me a beer in return Nicolás Lehmann
+ * -----------------------------------------------------------------------------
+ */
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+#define MAX 10000000
+
+int main() {
+  long long n;
+  cin >> n;
+  vector<long long> V(MAX+1);
+
+  V[1] = 1;
+  V[2] = 2;
+  if (n <= 2) {
+    cout << n << "\n";
+    return 0;
+  }
+  long i = 3;
+  long g = 2;
+  // cerr << 1 << " " << 2;
+  while (true) {
+    V[i] = V[i-1] + g;
+    // cerr << " " << V[i];
+    if (V[i] > n)
+      break;
+    if (i >= V[g+1]) ++g;
+    ++i;
+  }
+  // cerr << " ";
+  cout << i - 1 << "\n";
+
+  return 0;
+}
